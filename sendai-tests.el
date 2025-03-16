e19d1da9f133a6f793ac462069ccd9e24b41cfc5
Jim Porter <jporterbugs@gmail.com>
2025-03-15 17:31:01 -0700

    Update copyright

--- sendai-tests.el
+++ sendai-tests.el
@@ -1,6 +1,6 @@
 ;;; sendai-tests.el --- Tests for sendai -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021-2024 Jim Porter
+;; Copyright (C) 2021-2025 Jim Porter
 
 ;; Author: Jim Porter
 ;; Keywords: tests
